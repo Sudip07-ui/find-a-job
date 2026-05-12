f2b556321f04201d77f95438d6db9e56f81aef5e
Sudip_Rijal <sudiprijal7787@gmail.com>
2026-05-12 22:19:31 +0545

    got

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,5 +4,13 @@ const { registerUser, loginUser } = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+DynamicsCompressorNode;
+vsdvsdfv;
+sdvsdvsdv;
+vxcvsdf;sefsfrsrf
+sdfsfds;
+
+vdsfvcv ;
+sdfsdf;
 
 module.exports = router;
